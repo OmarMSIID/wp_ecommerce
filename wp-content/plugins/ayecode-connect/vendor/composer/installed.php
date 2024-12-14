2ee22050d89680de9458abb47a478fe9cd2fbe75
chammAyoub <ayoub.chammakh.23@edu.uiz.ac.ma>
2024-12-14 03:55:15 +0000

    ajouter la page Mon compte pour modifier les donnes de l'utilisateur et supprimer les pages : - A propos. - Services.

---
+++ wp-content/plugins/ayecode-connect/vendor/composer/installed.php
@@ -0,0 +1,71 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ayecode-connect/ayecode-connect',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7bffe26b109001190d3a73b7a409e136ebcc257c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ayecode-connect/ayecode-connect' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7bffe26b109001190d3a73b7a409e136ebcc257c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'ayecode/wp-ayecode-ui' => array(
+            'pretty_version' => '0.2.30',
+            'version' => '0.2.30.0',
+            'reference' => '3c9d60b1ce980ba7985e85cd58fb684b04270cd1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ayecode/wp-ayecode-ui',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'ayecode/wp-font-awesome-settings' => array(
+            'pretty_version' => '1.1.7',
+            'version' => '1.1.7.0',
+            'reference' => 'f52f48d05ff760eb1e22e747a2d011c669f25ed1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ayecode/wp-font-awesome-settings',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'ayecode/wp-super-duper' => array(
+            'pretty_version' => '1.2.16',
+            'version' => '1.2.16.0',
+            'reference' => '507e0cdf97c76fb5956c5a508624d586888e8d31',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ayecode/wp-super-duper',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'composer/installers' => array(
+            'pretty_version' => '1.x-dev',
+            'version' => '1.9999999.9999999.9999999-dev',
+            'reference' => '894a0b5c5d34c88b69b097f2aae1439730fa6836',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'roundcube/plugin-installer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'shama/baton' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+    ),
+);
